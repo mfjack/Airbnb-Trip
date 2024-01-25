15af3eda9c24c05e1c0578c4e1395a6b85f639c9
Marlon Ferreira <ferreira.marlon@live.com>
2024-01-25 20:24:57 -0300

    Update CategoryInput.tsx

--- airbnb-clone/app/components/input/CategoryInput.tsx
+++ airbnb-clone/app/components/input/CategoryInput.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 interface CategoryInputProps {
+   IconComponent: React.FC<{ size: number }>;
    label: string;
    selected?: boolean;
    onClick: (value: string) => void;
 }
 
 const CategoryInput: React.FC<CategoryInputProps> = ({
+   IconComponent,
    label,
    selected,
    onClick,
@@ -17,6 +19,7 @@ const CategoryInput: React.FC<CategoryInputProps> = ({
          className={` rounded-xl border-2 p-4 flex flex-col gap-3 hover:border-black transition cursor-pointer ${selected ? "border-black" : "border-neutral-200"}
    `}
       >
+         <IconComponent size={25} />
          <div className="font-semibold">{label}</div>
       </div>
    );

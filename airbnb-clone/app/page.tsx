4f910a660f1d4ed7d89938322b9ec71f920cb411
Marlon Ferreira <ferreira.marlon@live.com>
2024-01-30 11:09:58 -0300

    feat: Create listing card with location and price in the home.

--- airbnb-clone/app/page.tsx
+++ airbnb-clone/app/page.tsx
@@ -1,11 +1,15 @@
+import getCurrentUser from "./actions/getCurrentUser";
+import getListings from "./actions/getListings";
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
+import ListingCard from "./components/listings/ListingCard";
 import EmptyState from "./components/navbar/EmptyState";
 
-export default function Home() {
-    const isEmpty = true;
+export default async function Home() {
+    const listings = await getListings();
+    const currentUser = await getCurrentUser();
 
-    if (isEmpty) {
+    if (listings.length === 0) {
         return (
             <ClientOnly>
                 <EmptyState showReset />
@@ -17,7 +21,15 @@ export default function Home() {
         <ClientOnly>
             <Container>
                 <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-                    <div>Meus anúncios</div>
+                    {listings.map((listing: any) => {
+                        return (
+                            <ListingCard
+                                key={listing.id}
+                                currentUser={currentUser}
+                                data={listing}
+                            />
+                        );
+                    })}
                 </div>
             </Container>
         </ClientOnly>

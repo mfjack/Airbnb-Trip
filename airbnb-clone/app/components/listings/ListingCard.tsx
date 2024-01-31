dbad8d3879881f108d524478410991cd3a14b378
Jack <ferreira.marlon@live.com>
2024-01-31 18:34:27 -0300

    feat: finish include listing page individual with, category, info, client.

--- airbnb-clone/app/components/listings/ListingCard.tsx
+++ airbnb-clone/app/components/listings/ListingCard.tsx
@@ -33,6 +33,7 @@ const ListingCard: React.FC<ListingCardProps> = ({
     const { getByValue } = useCountries();
 
     const location = getByValue(data.locationValue);
+
     const handleCancel = useCallback(
         (e: React.MouseEvent<HTMLButtonElement>) => {
             e.stopPropagation();
@@ -68,7 +69,7 @@ const ListingCard: React.FC<ListingCardProps> = ({
     return (
         <>
             <div
-                onClick={() => router.push(`/listing/${data.id}`)}
+                onClick={() => router.push(`/listings/${data.id}`)}
                 className="col-span-1 cursor-pointer group"
             >
                 <div className="flex flex-col gap-2 w-full">
--- airbnb-clone/app/listings/[listingId]/page.tsx
+++ airbnb-clone/app/listings/[listingId]/page.tsx
@@ -1,7 +1,33 @@
-const ListingPage = () => {
-    return ( 
-        <div>Minha página de listagem individual</div>
-     );
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import getListingById from "@/app/actions/getListingById";
+import ClientOnly from "@/app/components/ClientOnly";
+import EmptyState from "@/app/components/navbar/EmptyState";
+import ListingClient from "./ListingClient";
+
+interface IParams {
+    listingId?: string;
 }
- 
+
+const ListingPage = async ({ params }: { params: IParams }) => {
+    const listing = await getListingById(params);
+    const currentUser = await getCurrentUser();
+
+    if (!listing) {
+        return (
+            <ClientOnly>
+                <EmptyState />
+            </ClientOnly>
+        );
+    }
+
+    return (
+        <ClientOnly>
+            <ListingClient
+                listing={listing}
+                currentUser={currentUser}
+            />
+        </ClientOnly>
+    );
+};
+
 export default ListingPage;

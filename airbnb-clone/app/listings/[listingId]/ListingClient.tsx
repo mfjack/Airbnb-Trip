4b60dc75e6441a3420009bdf7b65b85e29575012
Jack <ferreira.marlon@live.com>
2024-02-02 18:46:01 -0300

    feat: Criação de reserva de proprietários.

--- airbnb-clone/app/listings/[listingId]/ListingClient.tsx
+++ airbnb-clone/app/listings/[listingId]/ListingClient.tsx
@@ -1,19 +1,18 @@
 "use client";
 
 import { Range } from "react-date-range";
-import axios from "axios";
 import toast from "react-hot-toast";
-import Container from "@/app/components/Container";
-import ListingHead from "@/app/components/listings/ListingHead";
-import ListingInfo from "@/app/components/listings/ListingInfo";
-import { categories } from "@/app/components/navbar/Categories";
-import useLoginModal from "@/app/hooks/useLoginModal";
-import { SafeListing, SafeUser } from "@/app/types";
-import { Reservation } from "@prisma/client";
+import axios from "axios";
 import { differenceInCalendarDays, eachDayOfInterval } from "date-fns";
 import { useRouter } from "next/navigation";
 import { useCallback, useEffect, useMemo, useState } from "react";
+import { SafeListing, SafeReservation, SafeUser } from "@/app/types";
+import { categories } from "@/app/components/navbar/Categories";
+import Container from "@/app/components/Container";
+import ListingHead from "@/app/components/listings/ListingHead";
+import ListingInfo from "@/app/components/listings/ListingInfo";
 import ListingReservation from "@/app/components/listings/ListingReservation";
+import useLoginModal from "@/app/hooks/useLoginModal";
 
 const initialDateRange = {
     startDate: new Date(),
@@ -22,7 +21,7 @@ const initialDateRange = {
 };
 
 interface ListingClientProps {
-    reservations?: Reservation[];
+    reservations?: SafeReservation[];
     listing: SafeListing & {
         user: SafeUser;
     };
